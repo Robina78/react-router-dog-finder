97fb51cafd983ccbae2eb475a6add5af2c0b12f3
Ruby Hoseinzadeh <robina.sre78@gmail.com>
2021-09-27 13:36:47 -0700

    dog finder exercise

---
+++ src/DogList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "./DogList.css";
+
+
+const DogList = ({dogs}) => {
+    return (
+        <div className="DogList">
+            <div className="row mt-4">
+                <div className="col">
+                    <h1 className="text-center">
+                        HELLOZ. WE HAVE DOGZ. CLICK ON THEM FOR MORE INFO.
+                    </h1>
+                </div>
+            </div>
+            <div className="row">
+                {dogs.map(dog => (
+                    <div className="col-3 text-center" key={dog.name}>
+                        <img src={dog.src} alt={dog.name} />
+                        <h3 className="mt-3">
+                            <Link to={`/dogs/${dog.name.toLowerCase()}`}>{dog.name}</Link>
+                        </h3>
+                    </div>    
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default DogList;
